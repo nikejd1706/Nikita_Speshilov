9bf0318c766801d024178494df436e4d0c6b474e
nikejd1706 <nikspeshilov@gmail.com>
2016-09-07 09:40:20 -0700

    completed lab_02

--- PyLesson_02/Lab_02.py
+++ PyLesson_02/Lab_02.py
@@ -9,11 +9,24 @@ c = a*b
 print("12 and 9 produce" , c)
 
 
-name = Nikita
-address = 5035 ashley Falls Ct.
-city = San Diego
-zipcode = 92130
-print(name,address, city, zipcode)
+name = "Nikita Speshilov"
+address = "5035 Ashley Falls Ct."
+city = "San Diego"
+zipcode = "92130"
+print(name)
+print(address)
+print(city, zipcode)
+
+
+w = 5
+l = 10
+h = 6
+surArea = 2*w*l + 2*l*h + 2*h*w
+print("The surface area of your rectangle is" , surArea)
+
+
+
+
 
 
 
