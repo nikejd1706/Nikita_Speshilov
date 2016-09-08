cfa4c44117688edf81fe874578cc68944810df21
nikejd1706 <nikspeshilov@gmail.com>
2016-09-08 10:25:33 -0700

    Added lesson_03

---
+++ PyLesson_03/Lesson_03 practice.py
@@ -0,0 +1,9 @@
+num1 = int (input ("please enter your age: "))
+num2 = int (input ("please enter your mom's age: "))
+
+print("Wow! She was " , num2-num1, "when she had you.")
+
+              
+
+
+
