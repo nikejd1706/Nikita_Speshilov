86a625e42fa23d771163a2c42c8cb4ab3872070f
nikejd1706 <nikspeshilov@gmail.com>
2016-09-12 10:25:09 -0700

    added quiz material

--- Quizzes/ProgrammingQuiz_01StudyGuide.py
+++ Quizzes/ProgrammingQuiz_01StudyGuide.py
@@ -1,2 +1,35 @@
+print("question_01")
 num = 45
-int(print("IronMan has",num,"kinds of weapons in his suit"))
+print("IronMan has",num,"kinds of weapons in his suit.")
+print("question_02")
+pres = "Abraham Lincoln"
+quote = '"Whatever you are, be a good one."'
+print(pres,"once said,",quote,)
+print("question_03")
+h = 45
+w = 64
+print("The area of your rectangle is...",h*w,)
+print("question_04")
+print("When you enter seven instead of 7, you are given an error because you enter int(input) meaning that the input is expected to be a numerical value because of int.")
+print("question_05")
+print("The final output: Is there a Mr. or Mrs. Buster Brown?")
+print("question_06")
+print("The most likely output of the program: How many siblings do you have? >> 3    -The program produces an error after the first input")
+print("question_07")
+print("R. Kelly said "'"I believe I can fly!"')
+print("The line I believe I can fly isn't part of the statement because the quotations are not used correctly. To do a quotation, you use '"."' "")
+"'
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
