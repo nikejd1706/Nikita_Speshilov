e240509a89e743c5ff2c5c673caf6d4e9b110020
nikejd1706 <nikspeshilov@gmail.com>
2016-09-15 09:42:50 -0700

    finished study guide

--- Quizzes/ProgrammingQuiz_01StudyGuide.py
+++ Quizzes/ProgrammingQuiz_01StudyGuide.py
@@ -16,19 +16,14 @@ print("The final output: Is there a Mr. or Mrs. Buster Brown?")
 print("question_06")
 print("The most likely output of the program: How many siblings do you have? >> 3    -The program produces an error after the first input")
 print("question_07")
-print("R. Kelly said "'"I believe I can fly!"')
-print("The line I believe I can fly isn't part of the statement because the quotations are not used correctly. To do a quotation, you use '"."' "")
-"'
-
-
-      
-
-
-
-
-
-
-
+print("R. Kelly said \"I believe I can fly!\"")
+print("The line I believe I can fly isn't part of the statement because the quotations are not used correctly. To do a quotation, you use \ for starting and ending quotes")
+print("question_08")
+print("This one goes on top\nThis one goes on bottom")
+print("question_09")
+print("a\tb\tc\nd\te\tf\ng\th\ti")
+print("question_10")
+math = int(input("Please enter Math grade: "))
 
 
 
