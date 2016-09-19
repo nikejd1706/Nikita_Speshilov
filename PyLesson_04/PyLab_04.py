b4a52c1ce21543007e4829839769c549782d93e3
nikejd1706 <nikspeshilov@gmail.com>
2016-09-19 10:25:38 -0700

    added lab 04 and finished lesson

---
+++ PyLesson_04/PyLab_04.py
@@ -0,0 +1,18 @@
+def printf(word, number):
+    print("{:<15}\t{:10.2f}".format(word,number))
+item1 = input("Please enter item 1:")
+price1 = int(input("Please enter the price:"))
+
+item2 = input("Please enter item 2:")
+price2 = int(input("Please enter the price:"))
+
+item3 = input("Please enter item 3:")
+price3 = int(input("Please enter the price:"))
+
+subtotal = item1+item2+item3
+tax = 1.0875
+total = print("Your total is"subtotal/tax".")
+
+
+
+
--- PyLesson_04/PyLesson_04.py
+++ PyLesson_04/PyLesson_04.py
@@ -1,11 +1,17 @@
 
 
+def printf(word, number):
+    print("{:<6}\t{:10.2f}".format(word, number))
+
 word = "blah!"
+number = 69789.312
 
 printf(word, number)
 
-def printf(word, number):
-    print("{:<6}\t{:10.2f}".format(word, number))
+word = "ohboy!"
+number = 8879653.24858
+
+printf(word, number)
     
 
 
