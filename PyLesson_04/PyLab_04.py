084488d7bfcf1ef4b0d58965fe4431176e3e78e4
nikejd1706 <nikspeshilov@gmail.com>
2016-09-20 10:24:17 -0700

    renamed lesson 1 labs and continued working on lab_04

--- PyLesson_04/PyLab_04.py
+++ PyLesson_04/PyLab_04.py
@@ -1,18 +1,26 @@
-def printf(word, number):
-    print("{:<15}\t{:10.2f}".format(word,number))
+def format (item, price):
+    print("{:<15}\t{:10.2f}".format(item, price))
 item1 = input("Please enter item 1:")
-price1 = int(input("Please enter the price:"))
+price1 = float(input("Please enter the price:"))
 
 item2 = input("Please enter item 2:")
-price2 = int(input("Please enter the price:"))
+price2 = float(input("Please enter the price:"))
 
 item3 = input("Please enter item 3:")
-price3 = int(input("Please enter the price:"))
+price3 = float(input("Please enter the price:"))
 
-subtotal = item1+item2+item3
-tax = 1.0875
-total = print("Your total is"subtotal/tax".")
+subtotal = price1+price2+price3
+tax = 0.075
+tax1 = subtotal*tax
+print("<<<<<receipt>>>>>")
+format(item1, price1)
+format(item2, price2)
+format(item3, price3)
 
-
+format("Subtotal: ", subtotal)
+format("Tax: ", tax1)
+format("Total: ", subtotal+tax1)
+print("___________________________")
+print("Thank you for your support!")
 
 
