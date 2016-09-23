023e2cb90913637462c16e10aa1d5d60f8b8ad33
nikejd1706 <nikspeshilov@gmail.com>
2016-09-23 10:10:46 -0700

    finished lab

--- PyLesson_04/PyLab_04CompoundInterest.py
+++ PyLesson_04/PyLab_04CompoundInterest.py
@@ -1,12 +1,11 @@
-def payment(a,p,r,n,t):
-    output = "{:5.5f}".format(number)
-    return output
+def payment(p,r,n,t):
+    return ((p*((1+r/n)**(n*t)))/(t*12));
 
-p = float(input("Please enter the principal:"))
-r = float(input("Please enter the interest rate:"))
-n = float(input("Please enter the number of times the loan is compounded each year:"))
-t = float(input("Please enter the life of the loan in years:"))
-print("Your total payment is",p*(1+r/n)^n*t,".")
+P = float(input("Please enter the principal:"))
+R = float(input("Please enter the interest rate:"))
+N = float(input("Please enter the number of times the loan is compounded each year:"))
+T = float(input("Please enter the life of the loan in years:"))
+print("Your total payment is",payment(P,R,N,T),".")
           
 
 
