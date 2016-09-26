95e4de07f8ff9256e04b7cd702863f51a776164f
nikejd1706 <nikspeshilov@gmail.com>
2016-09-26 09:53:32 -0700

    revised files

--- PyLesson_04/PyLab_04CompoundInterest.py
+++ PyLesson_04/PyLab_04CompoundInterest.py
@@ -1,11 +1,11 @@
 def payment(p,r,n,t):
-    return ((p*((1+r/n)**(n*t)))/(t*12));
+    return (p+(p*((1+r/n)**(n*t)))/(t*12));
 
 P = float(input("Please enter the principal:"))
 R = float(input("Please enter the interest rate:"))
 N = float(input("Please enter the number of times the loan is compounded each year:"))
 T = float(input("Please enter the life of the loan in years:"))
-print("Your total payment is",payment(P,R,N,T),".")
+print("Your total payment is%8.2f"%payment(P,R,N,T),)
           
 
 
---
+++ PyLesson_04/PyLab_04SubwooferBoxes.py
@@ -0,0 +1,11 @@
+def volume(H,L,W):
+    return(H*L*W/12)
+
+H = float(input("Please enter the height of the subwoofer in cubic inches:"))
+L = float(input("Please enter the length of the subwoofer in cubic inches:"))
+W = float(input("Please enter the width of the subwoofer in cubic inches:"))
+print("The subwoofer volume is%8.2F"%volume(H,L,W),"cubic feet.")
+            
+
+
+
