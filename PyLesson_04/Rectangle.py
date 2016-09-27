a3229450c63c556866857151e1597eca14139fe7
nikejd1706 <nikspeshilov@gmail.com>
2016-09-27 10:19:34 -0700

    added rectangle file

---
+++ PyLesson_04/Rectangle.py
@@ -0,0 +1,10 @@
+def perimeter(length,width):
+    return((length+width)*2)
+
+
+
+length = float(input("Please enter the length of your rectangle:"))
+width = float(input("Please enter the width of your rectangle:"))
+print("Your rectangle is%8.2F"%perimeter(length,width),"sq ft around.")
+
+          
