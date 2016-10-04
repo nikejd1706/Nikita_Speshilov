510c000ca15efa9944ac2132d52b0cbee9785d00
nikejd1706 <nikspeshilov@gmail.com>
2016-10-04 10:22:38 -0700

    added file

---
+++ PyLesson_04/Cube_2.py
@@ -0,0 +1,9 @@
+def setNums():
+    global side
+def surfacearea():
+    global sum
+    sum = ((side**2)*6)
+    output = "{:<6}{:6.2f}".format(side)
+
+side = float(input("Please enter the side of a cube to calculate the surface area
+    
