a685e87a0352cba56bbdbf79901b33ff60dea586
nikejd1706 <nikspeshilov@gmail.com>
2016-10-11 10:23:29 -0700

    revised file

--- PyLesson_05/Exercise_02.py
+++ PyLesson_05/Exercise_02.py
@@ -1,14 +1,24 @@
-def format (item, price):
-    print("{:<15}{:10.2f}".format(item, price))
+global subtotal, disc, price
+subtotal = 0;
+disc = 0;
+price = 0;
 
-def discount1():
-    global price
+def format (item, pr):
+    print("*  {:<15}......${:10.2f}".format(item, pr))
+
+def discount():
+    #use the global keyword here to ensure that
+    #python is using the global variable and not
+    #creating a new one
+    global subtotal, disc, price
     if price >= 2000:
-        return "Your discount is",subtotal^.15,"$"
+        disc = subtotal**0.15;
+        return "Your discount is $",subtotal**.15;
     if price < 2000:
-        return "0$"
+        return "$0"
+        disc = 0;
 
-    
+
 item1 = input("Please enter item 1:")
 price1 = float(input("Please enter the price:"))
 
@@ -22,9 +32,9 @@ item4 = input("Please enter item 4:")
 price4 = float(input("Please enter the price:"))
 
 subtotal = price1+price2+price3+price4
-discount = discount1()
-tax = (subtotal-discount)^.08
-total = subtotal-discount+tax
+discount();
+tax = subtotal*.08
+total = subtotal- disc + tax
 
 print("          <<<<<_receipt_>>>>>          ")
 format(item1, price1)
@@ -33,7 +43,7 @@ format(item3, price3)
 format(item4, price4)
 
 format("Subtotal: ", subtotal)
-format("Discount: ", discount)
+format("Discount: ", disc)
 print("_________________________________________")
 print("Thank you for shopping!")
 
