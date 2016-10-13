86d8361bf9ca7cc3412b48d7dfcbecc3be49a42e
nikejd1706 <nikspeshilov@gmail.com>
2016-10-12 21:22:53 -0700

    added files

--- PyLesson_05/PyLab_05.1_GPAcalculator.py
+++ PyLesson_05/PyLab_05.1_GPAcalculator.py
@@ -1,39 +1,19 @@
 
 GPA = 0;
 
-def calcPoints():
-    global lettergrade1, lettergrade2, lettergrade3, lettergrade4, lettergrade5, lettergrade6, lettergrade7
+def calcPoints(lettergrade):
 
-if lettergrade1 == A:
+if lettergrade == A:
     return "4.0"
-elif lettergrade1 == B:
+elif lettergrade == B:
     return "3.0"
-elif lettergrade1 == C:
+elif lettergrade == C:
     return "2.0"
-elif lettergrade1 == D:
+elif lettergrade == D:
     return "1.0"
-elif lettergrade1 == F:
+elif lettergrade == F:
     return "0.0"
-if lettergrade2 == A:
-    return "4.0"
-elif lettergrade2 == B:
-    return "3.0"
-elif lettergrade2 == C:
-    return "2.0"
-elif lettergrade2 == D:
-    return "1.0"
-elif lettergrade2 == F:
-    return "0.0"
-if lettergrade3 == A:
-    return "4.0"
-elif lettergrade3 == B:
-    return "3.0"
-elif lettergrade3 == C:
-    return "2.0"
-elif lettergrade3 == D:
-    return "1.0"
-elif lettergrade3 == F:
-    return "0.0"
+
  
 lettergrade1 = input("Please enter your letter grade for math: ")
 lettergrade2 = input("Please enter your letter grade for science: ")
@@ -43,7 +23,7 @@ lettergrade5 = input("Please enter your letter grade for foreign language: ")
 lettergrade6 = input("Please enter your letter grade for elective: ")
 lettergrade7 = input("Please enter your letter grade for physical education: ")
 
-GPA = (lettergrade1+lettergrade2+lettergrade3+lettergrade4+lettergrade5+lettergrade6+lettergrade7)/7
+GPA = (calcPoints(lettergrade1)+calcPoints(lettergrade2)+calcPoints(lettergrade3)+calcPoints(lettergrade4)+lettergrade5+lettergrade6+lettergrade7
 
 
 print("Your GPA is",GPA)
