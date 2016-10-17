103b80ca7d8327b2f07cb297ed64e9e70333e127
nikejd1706 <nikspeshilov@gmail.com>
2016-10-17 10:14:12 -0700

    finished calculator!!!

--- PyLesson_05/PyLab_05.1_GPAcalculator.py
+++ PyLesson_05/PyLab_05.1_GPAcalculator.py
@@ -1,13 +1,13 @@
 def calcPoints(grde):
-    if grde == "A" or "a":
+    if grde == "a":
         return 4.0
-    if grde == "B" or "b":
+    if grde == "b":
         return 3.0
-    if grde == "C" or "c":
+    if grde == "c":
         return 2.0
-    if grde == "D" or "d":
+    if grde == "d":
         return 1.0
-    if grde == "F" or "f":
+    if grde == "f":
         return 0.0
     
     
@@ -23,4 +23,4 @@ g5 = input("Please enter your letter grade for foreign language: ")
 g6 = input("Please enter your letter grade for elective: ")
 g7 = input("Please enter your letter grade for physical education: ")
 
-print("Your GPA is", (calcPoints(g1)+calcPoints(g2)+calcPoints(g3)+calcPoints(g4)+calcPoints(g5)+calcPoints(g6)+calcPoints(g7)/7)
+print("Your GPA is", (calcPoints(g1)+calcPoints(g2)+calcPoints(g3)+calcPoints(g4)+calcPoints(g5)+calcPoints(g6)+calcPoints(g7))/7)
