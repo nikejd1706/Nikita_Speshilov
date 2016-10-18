004ca24994e33e0574395293a9522608247c9c4d
nikejd1706 <nikspeshilov@gmail.com>
2016-10-18 10:23:42 -0700

    revised files

---
+++ PyLesson_05/BMI_Part2.py
@@ -0,0 +1,26 @@
+bmi = 0
+num = 703
+def calcBMI(bmi):
+    return "Your BMI is", weight/height**2*num
+    if bmi < 18.5
+        return "underweight"
+    elif bmi >= 24.9
+        return "normal"
+    elif bmi >= 29.9
+        return "overweight"
+    elif bmi >= 34.9
+        return "obese"
+    elif bmi >= 39.9
+        return "very obese"
+    elif bmi > 39.9
+        return "morbidly obese"
+
+     
+    
+
+
+
+
+weight = input(print("Please enter your height: "))
+height = input(print("Please enter your weight: "))
+      
