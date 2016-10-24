0536dbb47f8f96813fd177ccc94951534f862df8
nikejd1706 <nikspeshilov@gmail.com>
2016-10-24 10:25:27 -0700

    revised files

---
+++ PyLesson_05/Exercise_05.2_AdventureGame.py
@@ -0,0 +1,39 @@
+print("We are going on an adventure!")
+adventure = input("You walk into the forest, do you go..." +
+                        "\n 1. left" +
+                        "\n 2. right")
+
+
+      
+
+
+
+if adventure == 1:
+    left = input("You encounter a demon, how do you proceed....?" +
+                       "\n 3. ignore it" + 
+                       "\n 4. attack it")
+    if choice == 3
+        if choice == :
+            #
+        else:
+            #
+    else
+        if choice == c:
+            #
+        else
+            #
+else:
+    right = ("You encounter a river up ahead, do you...." +
+             "\n 1. Swim through it" +
+             "\n 2. Attempt to build a boat")
+    if choice == a:
+        if choice == b:
+            #
+        else:
+            #
+    else:
+        if choice == c:
+            #
+        else:
+            #
+            
