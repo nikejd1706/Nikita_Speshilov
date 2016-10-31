07340bacff06f43408ee2d9e5a4f6b2020c8662d
nikejd1706 <nikspeshilov@gmail.com>
2016-10-31 10:24:48 -0700

    finished program

--- PyLesson_05/Exercise_05.2_PasswordProgram.py
+++ PyLesson_05/Exercise_05.2_PasswordProgram.py
@@ -1,13 +1,28 @@
-password = 0;
-
-username = input("Please choose a username: ")
-password = input("Please choose a password: ")
+username1 = "robot"
+password1 = "jump"
 
 def passCheck():
-    global username, password
-    if username and password == username and password
-        return "You are granted access!"
-    if username 
+    username = input("Please enter the username: ")
+    password = input("Please enter the password: ")
+    
+    if username == username1 and password == password1:
+        print("You are granted access!")
+    else:
+        if username == username1:
+            
+            print("Your password is incorrect!")
+            passCheck()
+        elif password == password1:
+            print("Your username is incorrect!")
+            passCheck()
+        else:
+            print("Your username and password are incorrect!")
+            passCheck()
+
+passCheck()
+
+
+
     
     
     
