643e12517ff71bd8d7f7aa5e5c80d1082631948e
nikejd1706 <nikspeshilov@gmail.com>
2016-11-15 10:24:58 -0800

    did stuff

--- PyLesson_08/Center.py
+++ PyLesson_08/Center.py
@@ -1,1 +1,10 @@
-word
+word1 = input("Please enter the first word: ")
+word2 = input("Please enter the second word: ")
+word3 = input("Please enter the third word: ")
+
+def makeCenter(word):
+    if len(word1)>= 20:
+        return (word1)
+    else:
+        return word
+        
