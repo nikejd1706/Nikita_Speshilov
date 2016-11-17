a66e24448001db08f787c226f8e5e9da8b155fc4
nikejd1706 <nikspeshilov@gmail.com>
2016-11-17 10:23:14 -0800

    added files

--- PyLesson_08/Center.py
+++ PyLesson_08/Center.py
@@ -3,8 +3,11 @@ word2 = input("Please enter the second word: ")
 word3 = input("Please enter the third word: ")
 
 def makeCenter(word):
-    if len(word1)>= 20:
-        return (word1)
-    else:
+    if len(word)>= 20:
         return word
+    else:
+        return " " + word + " "
+print(makeCenter(word1))
+print(makeCenter(word2))
+print(makeCenter(word3))
         
---
+++ PyLesson_08/Lucky7s.py
@@ -0,0 +1,11 @@
+number = int(input("Please enter a number: "))
+
+def luck(number):
+    if number > 0:
+        if
+            return 1 + luck(number)/10
+        else:
+            return 0 + luck(number)/10
+    else:
+        return 0
+        
