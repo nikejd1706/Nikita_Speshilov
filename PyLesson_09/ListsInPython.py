dd9afe22e96803d96fff7f9ca905e8ed3cb92b53
nikejd1706 <nikspeshilov@gmail.com>
2016-11-28 10:23:47 -0800

    added file

---
+++ PyLesson_09/ListsInPython.py
@@ -0,0 +1,16 @@
+myList = [1, 2, 3, 4, 5]
+
+print(myList[2])
+print(myList[:2])
+print(myList[2:3])
+print(myList)
+
+output = ""
+j = 0
+for i in myList:
+    output += str(i)
+    if j < len(myList)-1:
+        output += ", "
+    j+=1
+
+print(output)
