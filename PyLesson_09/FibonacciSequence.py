a479e6fb273d412de868c219d9d58d44012caf58
nikejd1706 <nikspeshilov@gmail.com>
2016-12-09 10:24:42 -0800

    add

--- PyLesson_09/FibonacciSequence.py
+++ PyLesson_09/FibonacciSequence.py
@@ -3,10 +3,12 @@ size = int(input("Please enter your sequence size: "))
 
 seq = []
 
-for i in range(0, size)
-    if i == 0 or 1:
-        seq
+for i in range(0, size):
+    if i == 0 or i == 1:
+        seq.append(start)
     else:
+        seq.append(start                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+        print(seq[i] + " ")
         
         
         
