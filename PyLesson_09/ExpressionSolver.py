b45a6c841c3b728e31652beaa98f55c75be8bce8
nikejd1706 <nikspeshilov@gmail.com>
2016-12-15 10:22:22 -0800

    added files

---
+++ PyLesson_09/ExpressionSolver.py
@@ -0,0 +1,12 @@
+expression = int(input("Please enter a mathematical expression: "))
+
+equation = expression.split(" ")
+output = ""
+for i in expression:
+    output += (i)
+print(output)
+
+while i < len(equation):
+    if i < len(equation) and equation.append("*", "/")
+    
+
--- PyLesson_09/KeepComposites.py
+++ PyLesson_09/KeepComposites.py
@@ -1,11 +1,11 @@
+
 nums = [2,6,8,9,10,12,13,15,17,24,55,66,78,77,79]
 
 def gFactor(number):
     for i in range(2, number):
-        if number/(i):
+        if number%(i) == 0:
             return 1
-        else:
-            return 0
+    return 0
 def removePrimes():
     for number in nums:
         if gFactor(number) == 0:
