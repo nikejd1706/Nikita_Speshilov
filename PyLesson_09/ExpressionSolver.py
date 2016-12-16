0c5dee8e6310c49ab23c6b1f69e963f0a8514812
nikejd1706 <nikspeshilov@gmail.com>
2016-12-16 10:24:22 -0800

    added file

--- PyLesson_09/ExpressionSolver.py
+++ PyLesson_09/ExpressionSolver.py
@@ -1,12 +1,34 @@
-expression = int(input("Please enter a mathematical expression: "))
-
+expression = input("Please enter a mathematical expression: ")
 equation = expression.split(" ")
-output = ""
-for i in expression:
-    output += (i)
-print(output)
+print(equation)
+
+i = 0
 
 while i < len(equation):
-    if i < len(equation) and equation.append("*", "/")
+    if i < len(equation) and (equation[i] == "*" or equation[i] == "/"):
+        if equation[i] == "*":
+            equation[i] = int(equation[i-1]) * int(equation[i+1])
+        else:
+            equation[i] = int(equation[i-1]) / int(i+1)
+        print(equation)
+        equation.pop(i-1)
+        equation.pop(i)
+    else:
+        i += 1
+
+i = 0
+
+while i < len(equation):
+    if i < len(equation) and (equation[i] == "+" or equation[i] == "-"):
+        if equation[i] == "+":
+            equation[i] = int(equation[i-1]) + int(equation[i+1])
+        else:
+            equation[i] = int(equation[i-1]) -int (equation[i+1])
+        equation.pop(i-1)
+        equation.pop(i)
+    else:
+        i += 1
+
+print(equation)
     
 
