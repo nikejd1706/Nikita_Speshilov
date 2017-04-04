a214987d0fc5065335dcb3039cb082cf67b2b95e
nikejd1706 <nikspeshilov@gmail.com>
2017-04-04 10:21:49 -0700

    added files

--- WebLesson_14/script.js
+++ WebLesson_14/script.js
@@ -1,9 +1,8 @@
 function validate() {
     var x = document.forms.input.username.value;
-    var a = document.forms.input.password.value;
+    var y = document.forms.input.password.value;
     var atPos = x.indexOf("@");
     var dotPos = x.lastIndexOf(".");
-    var pass = a.length;
 
 
     //we need... username@webAddress.extension
@@ -12,9 +11,16 @@ function validate() {
         //@ is in the wrong place
         //there is no .com, .gov, or any otg
         //final dot is in the wrong place
-    if(atPos < 1 || dotPos < atPos+2 || dotPos + 2 > x.length)
-        alert("This is not a valid email address!")
-
+    if ((atPos < 1 || dotPos < atPos + 2 || dotPos + 2 > x.length) && y.length < 6)//username and password are both incorrect
+        alert("Both username and password are incorrect");
     else
-        alert("Success!");
+    {
+        if (atPos < 1 || dotPos < atPos + 2 || dotPos + 2 > x.length)
+            alert("This is not a valid email address!");
+        else if (y.length < 6)
+            alert("Your password doesn't meet the minimum requirements");
+
+        else
+            alert("Success!");
+    }
 }
