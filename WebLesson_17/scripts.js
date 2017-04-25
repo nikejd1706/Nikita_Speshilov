cef8b1faef854854a2783434ff358d42087dfdfe
nikejd1706 <nikspeshilov@gmail.com>
2017-04-25 10:25:27 -0700

    added files

--- WebLesson_17/scripts.js
+++ WebLesson_17/scripts.js
@@ -1,3 +1,22 @@
-/**
- * Created by speshilovn0763 on 4/24/2017.
- */
+function drag() {
+    mantis = document.getElementById("mantisShrimp");
+    leftbox = document.getElementById("leftBox");
+
+    mantis.addEventListener("dragstart", startDrag, false);
+
+    leftbox.addEventListener("dragenter", function(e){e.preventDefault()}, false);
+    leftbox.addEventListener("dragover", function(e){e.preventDefault()}, false);
+    leftbox.addEventListener("drop", drop, false);
+}
+
+function startDrag(e){
+    var pic = '<img id = "mantisShrimp" src = "https://upload.wikimedia.org/wikipedia/commons/3/39/OdontodactylusScyllarus2.jpg">';
+    e.dataTransfer.setData('Picture', pic);
+}
+
+function drop(e){
+    e.preventDefault();
+    leftBox.innerHTML = e.dataTransfer.getData('Picture');
+}
+
+window.addEventListener("load", drag, false);
