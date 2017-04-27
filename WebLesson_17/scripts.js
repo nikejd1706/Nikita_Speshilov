cc2aafe2403d1df9a7647631464482b57cb7ffd4
nikejd1706 <nikspeshilov@gmail.com>
2017-04-27 10:26:14 -0700

    added files

--- WebLesson_17/scripts.js
+++ WebLesson_17/scripts.js
@@ -3,8 +3,10 @@ function drag() {
     leftbox = document.getElementById("leftBox");
 
     mantis.addEventListener("dragstart", startDrag, false);
+    mantis.addEventListener("dragend", endDrag, false);
 
-    leftbox.addEventListener("dragenter", function(e){e.preventDefault()}, false);
+    leftbox.addEventListener("dragenter", dragEnter, false);
+    leftbox.addEventListener("dragleave", dragLeave, false);
     leftbox.addEventListener("dragover", function(e){e.preventDefault()}, false);
     leftbox.addEventListener("drop", drop, false);
 }
@@ -14,10 +16,25 @@ function startDrag(e) {
     e.dataTransfer.setData('Picture', pic);
 }
 
-function drop(e)
-{
+function dragEnter(e) {
+    e.preventDefault();
+    leftbox.style.background = "#80637c";
+    leftbox.style.border = "3px solid green";
+}
+
+function dragLeave(e) {
+    e.preventDefault();
+    leftbox.style.background = "white";
+    leftbox.style.border = "3px solid purple";
+
+function drop(e) {
     e.preventDefault();
     leftBox.innerHTML = e.dataTransfer.getData('Picture');
+}
+
+function endDrag(e) {
+    pic = e.target
+    pic.style.visibility = "hidden";
 }
 
 window.addEventListener("load", drag, false);
